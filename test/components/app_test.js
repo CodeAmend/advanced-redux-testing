6b6cf5a9f2e41cc11e191e2ad4f20d0539b26680
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-12 15:06:54 -0400

    Working App test

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,1 +1,13 @@
+import {renderComponent, expect} from '../test_helper';
+import App from '../../src/components/app';
 
+describe('App', () => {
+
+  it('shows the correct text', () => {
+
+    const component = renderComponent(App);
+    expect(component).to.contain("React simple starter");
+
+  });
+
+});
