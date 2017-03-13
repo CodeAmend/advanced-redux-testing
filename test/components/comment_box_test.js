a727c295af4f2ca5d37e32d940ab6ac9aa652f47
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-12 22:52:40 -0400

    has button and contains text test

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -1,10 +1,40 @@
 import {renderComponent, expect} from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
-descrive("CommentBox", () => {
+describe("CommentBox", () => {
+  let component;
 
-  it("", () => {
-    
+  beforeEach(() => {
+    component = renderComponent(CommentBox);
+  });
+
+  it("has the correct className: comment-box", () => {
+    expect(component).to.have.class('comment-box');
+  });
+
+  // has a button
+  it("has a text area", () => {
+    expect(component.find('textarea')).to.exist;
+  });
+  // has a text area
+  it("has a button", () => {
+    expect(component.find('button')).to.exist;
+  });
+
+  describe("entering some text", () => {
+
+    beforeEach(() => {
+      component.find('textarea').simulate('change', "new comment");
+    });
+
+    it("shows text that is entered", () => {
+      expect(component.find('textarea')).to.have.value("new comment");
+    });
+
+    it("when submitted, clears the input", () => {
+      expect();
+    });
+
   });
 
 });
