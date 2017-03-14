e7473835b84b9d0374f1beffa4350023c23a4010
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-14 18:22:12 -0400

    Component list passes

--- src/components/comment_list.js
+++ src/components/comment_list.js
@@ -1,9 +1,19 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 const CommentList = (props) => {
+  const commentList = props.comments.map(
+    comment => <li key={comment}>{comment}</li>
+  );
   return (
-    <ul className="comment-list"></ul>
+    <ul className="comment-list">
+      {commentList}
+    </ul>
   );
 }
 
-export default CommentList;
+function mapStateToProps(state) {
+  return { comments: state.comments }
+}
+
+export default connect(mapStateToProps)(CommentList);
