aa47296703497a9536658eb0057864ce0b0c350d
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-03-15 12:23:47 -0500

    Added saveComment() action and testing passed

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -14,18 +14,28 @@ class CommentBox extends Component {
     this.setState({ comment: event.target.value });
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.saveComment(this.state.comment);
+
+    this.setState({ comment: '' });
+  }
+
   render() {
     return (
-      <div className="comment-box">
+      <form
+        onSubmit={this.handleSubmit.bind(this)}
+        className="comment-box"
+      >
         <textarea
           value={this.state.comment}
           onChange={this.handleChange.bind(this)}
         />
         <button
           action="submit"
-          onClick={}
         >Submit</button>
-      </div>
+      </form>
     );
   }
 
--- src/components/comment_list.js
+++ src/components/comment_list.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import React, {Component} from 'react';
 
 const CommentList = (props) => {
   const commentList = props.comments.map(
